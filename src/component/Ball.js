954ec7f3f77356a220c2bcbf8a6530fc11935d44
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-03-05 20:19:07 -0800

    rainbow star

--- src/component/Ball.js
+++ src/component/Ball.js
@@ -5,9 +5,10 @@ class Ball{
   constructor(args) {
     this.position = args.position
     this.velocity = {
-      x: 5,
-      y: 5
+      x: 10,
+      y: 10
     }
+    this.counter = 0;
 
   }
 
@@ -19,9 +20,11 @@ class Ball{
         frequency =5,
         red = Math.sin(frequency*(time%len) + 0) * width + center,
         green = Math.sin(frequency*(time%len) + 2) * width + center,
-        blue = Math.sin(frequency*(time%len)  + 4) * width + center;
+        blue = Math.sin(frequency*(time%len)  + 4) * width + center,
+        rainbow =  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].reverse();
 
 
+        this.counter++;
 
     if(this.position.x + this.velocity.x  > state.screen.width * state.screen.ratio - 40 || this.position.x + this.velocity.x  <  40 ){
       this.velocity.x = -this.velocity.x;
@@ -34,7 +37,7 @@ class Ball{
     const context = state.context;
     context.save()
     // context.clearRect(0, 0, state.screen.width, state.screen.height);
-    drawStar(context, this.position.x += this.velocity.x, this.position.y += this.velocity.y, 5, 70, 30, RGB2Color(red,green,blue));
+    drawStar(context, this.position.x += this.velocity.x, this.position.y += this.velocity.y, 5, 70, 30, rainbow[this.counter%8]);
   }
 }
 
